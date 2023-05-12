80ad956708b1058835563c4ff06fa844da493e06
PRANAV REDDY <46398827+PranavG200@users.noreply.github.com>
2023-05-12 12:34:36 -0400

    .

--- DuffingOscillator.py
+++
@@ -1,8 +0,0 @@
-# DUFFING OSCILLATOR
-def DuffingOscillatorDynamics(x, y, alpha, beta, delta):
-
-    timestep = 0.25
-    xdot = y
-    ydot = -delta*y - x*(alpha*(x**2) + beta)
-
-    return [x + timestep*xdot, y + timestep*ydot]
